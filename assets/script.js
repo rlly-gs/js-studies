06f81f9b5c426cec804f6e95c4047d97939d31f7
Gustavo Lacerda <rlly.gs@gmail.com>
2026-03-12 06:26:00 -0300

    if basics

---
+++ assets/script.js
@@ -0,0 +1,50 @@
+
+/*
+let n = Number(prompt("Digite um número: "))
+
+if (n % 2 === 0) {
+    console.log("par")
+} else {
+    console.log("impar")
+}
+*/
+
+/*
+let anoN = Number(prompt("Qual a sua idade: "))
+
+if (2026 - anoN > 18) {
+    console.log("Maior, " + `${ + 2026 - anoN + " anos de idade"}`)
+}
+else{
+    console.log("Menor, " + `${2026 - anoN + " anos de idade"}`)
+}
+*/
+
+
+/*
+let n = Number(prompt("Digite um número: "))
+
+if (n > 0) {
+    console.log("Positivo")
+} else if ( n === 0) {
+    console.log("Zero")
+} else {
+    console.log("Negativo")
+}
+*/
+
+/*
+let n1 = Number(prompt("Nota 1: "))
+let n2 = Number(prompt("Nota 2: "))
+let n3 = Number(prompt("Nota 3: "))
+
+let media = (n1 + n2 + n3) / 3;
+
+if (media >= 7.0) {
+    console.log("Aprovado, média: " + media)
+} else if (media >= 5 && media <= 6.9) {
+    console.log("Recuperação, média: " + media)
+} else {
+    console.log("Reprovado, média: " + media)
+}
+*/
