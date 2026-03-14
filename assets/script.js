e12bd600d1f002cde1b1be254f1f3f317ddca262
Gustavo Lacerda <rlly.gs@gmail.com>
2026-03-14 04:31:54 -0300

    function exercises

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,10 @@
-
-        //função
-function bhaskara (a, b, c) {
-
-    let delta = Math.pow(b, 2.0) - (4 * a * c);
-
-    if (delta < 0) {
-        console.log("Delta menor que zero.")
-    }else {
-        let x1 = (-b + Math.sqrt(delta)) / (2 * a)      //estrutura da função
-        let x2 = (-b - Math.sqrt(delta)) / (2 * a )
-        console.log(`Delta: ${delta}`)
-        console.log(`x1: ${x1}`)
-        console.log(`x2 ${x2}`)
+function parOuImpar (a) {
+    if (a % 0) {
+        console.log("par")
+    } else {
+        console.log("impar")
     }
 }
 
-bhaskara(1, -2, 5); //chama função
+const number = Number(prompt("Digite um número: "))
+parOuImpar(number)
