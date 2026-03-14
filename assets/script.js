fc3e67957d57613cbd75aaa65f7f6004658eadeb
Gustavo Lacerda <rlly.gs@gmail.com>
2026-03-14 04:42:28 -0300

    (3)

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,18 @@
-function maiorNumero (a, b) {
-    if (a > b) {
-        console.log("Primeiro número é maior.")
-    }  else {
-        console.log("Segundo número é maior")
+function analisaStatus(n1, n2, n3, n4) {
+    const media = (n1 + n2 + n3 + n4) / 4
+
+    if (media > 7.0) {
+        console.log(`Aprovado, média = ${media}`)
+    } else if (media >= 5.0 && media <= 6.9) {
+        console.log(`Recuperação, média = ${media}`)
+    } else {
+        console.log(`Reprovado, média = ${media}`)
     }
 }
 
-const number1 = Number(prompt("Digite o primeiro número: "))
-const number2 = Number(prompt("Digite o segundo número: "))
+const nota1 = Number(prompt("Digite a primeira nota: "))
+const nota2 = Number(prompt("Digite a segunda nota: "))
+const nota3 = Number(prompt("Digite a terceira nota: "))
+const nota4 = Number(prompt("Digite a quarta nota: "))
 
-maiorNumero(number1, number2)
+analisaStatus(nota1,nota2,nota3,nota4)
