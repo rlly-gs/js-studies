1b3cf54ead55682556c003f34c93ca36c23815bf
Gustavo Lacerda <rlly.gs@gmail.com>
2026-03-14 04:15:59 -0300

    about functions

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,18 @@
-let hour = prompt("Horário: ");
 
-switch(hour) {
-    case 'manhã':
-        console.log("Bom dia");
-        break;
-    case 'tarde':
-        console.log("Boa tarde");
-        break;
-    case 'noite':
-        console.log("Boa noite");
-        break;
+
+function bhaskara (a, b, c) {
+
+    let delta = Math.pow(b, 2.0) - (4 * a * c);
+    
+    if (delta < 0) {
+        console.log("Delta menor que zero.")
+    }else {
+        let x1 = (-b + Math.sqrt(delta)) / (2 * a)
+        let x2 = (-b - Math.sqrt(delta)) / (2 * a )
+        console.log(`Delta: ${delta}`)
+        console.log(`x1: ${x1}`)
+        console.log(`x2 ${x2}`)
+    }
 }
+
+bhaskara(1, -5, 6);
