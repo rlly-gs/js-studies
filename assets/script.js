d8c190de098aca80d48407e8ec6ef6cfe7c21c58
Gustavo Lacerda <rlly.gs@gmail.com>
2026-03-14 05:00:19 -0300

    with return

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,12 @@
-function analisaStatus(n1, n2, n3, n4) {
-    const media = (n1 + n2 + n3 + n4) / 4
-
-    if (media > 7.0) {
-        console.log(`Aprovado, média = ${media}`)
-    } else if (media >= 5.0 && media <= 6.9) {
-        console.log(`Recuperação, média = ${media}`)
-    } else {
-        console.log(`Reprovado, média = ${media}`)
+function verificarEntrada (idade) {
+    if (idade >= 18) {
+        return "Liberado"
     }
+    
+    return "Menor de idade"
 }
 
-const nota1 = Number(prompt("Digite a primeira nota: "))
-const nota2 = Number(prompt("Digite a segunda nota: "))
-const nota3 = Number(prompt("Digite a terceira nota: "))
-const nota4 = Number(prompt("Digite a quarta nota: "))
+const idade = Number(prompt("Idade = "))
+const permissao = verificarEntrada(idade)
 
-analisaStatus(nota1,nota2,nota3,nota4)
+console.log(permissao)
